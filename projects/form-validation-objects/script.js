b073c09fc13d14946b9e35f51da80f29874b77a3
Dan Codae <drcoda@gmail.com>
2018-08-27 18:15:10 -0400

    added field is required text to car year, make, model

--- projects/form-validation-objects/script.js
+++ projects/form-validation-objects/script.js
@@ -1,1 +1,51 @@
 console.log('Add validation!')
+
+document.getElementById('parking-form').addEventListener('submit', function (e) {
+  e.preventDefault()
+  clearError()
+  let inputElements = document.getElementsByTagName('input')
+  for (let element of inputElements) {
+    if (element.id === 'car-year' || element.id === 'car-make' || element.id === 'car-model') {
+      console.log(element.id)
+      if (element.value.trim() === '') {
+        element.parentElement.classList.add('input-invalid')
+        element.parentElement.classList.remove('input-valid')
+        carValid(element)
+      } else {
+        element.parentElement.classList.add('input-valid')
+        element.parentElement.classList.remove('input-invalid')
+      }
+    } else if (element.value.trim() === '') {
+      element.parentElement.classList.add('input-invalid')
+      element.parentElement.classList.remove('input-valid')
+      errorMessage(element.parentElement)
+    } else {
+      element.parentElement.classList.add('input-valid')
+      element.parentElement.classList.remove('input-invalid')
+    }
+  }
+})
+
+function errorMessage (element) {
+  let errorDiv = document.createElement('div')
+  errorDiv.classList.add('error-msg')
+  element.appendChild(errorDiv)
+
+  errorDiv.innerText = 'field is required'
+}
+
+function carValid (element) {
+  let carDiv = document.getElementById('car-field')
+  let errorDiv = document.createElement('div')
+  errorDiv.classList.add('error-msg')
+  carDiv.appendChild(errorDiv)
+
+  errorDiv.innerText = 'field is required'
+}
+
+function clearError () {
+  let errorMsgs = document.querySelectorAll('.error-msg')
+  for (let errorMsg of errorMsgs) {
+    errorMsg.remove()
+  }
+}
